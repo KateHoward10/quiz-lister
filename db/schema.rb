b7665eddeac21b4e32f3519b263d514d16dd1688
Kate Howard <k.l.howard.1992@gmail.com>
2019-07-08 19:55:43 +0100

    Switch from SQLite to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_16_161941) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "quizzes", force: :cascade do |t|
     t.string "venue"
     t.string "day"
