ef5e353212f9e077d1b03f2b76a0474ccaea2c62
Kate Howard <k.l.howard.1992@gmail.com>
2019-07-10 19:26:20 +0100

    Get photo url from placeId

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -19,7 +19,9 @@ function initMap2() {
   const postcodeInput = document.getElementById('quiz_postcode');
   const latInput = document.getElementById('quiz_latitude');
   const lngInput = document.getElementById('quiz_longitude');
+  const imageInput = document.getElementById('quiz_image');
   const geocodeButton = document.getElementById('geocode_button');
+  const image = document.getElementById('venue-image');
 
   let coords = latInput.value && lngInput.value ? new google.maps.LatLng(latInput.value, lngInput.value) : new google.maps.LatLng(51.45946, -2.5907347);
 
@@ -38,6 +40,13 @@ function initMap2() {
   });
 
   const geocoder = new google.maps.Geocoder;
+  const places = new google.maps.places.PlacesService(map);
+
+  function setPhoto(results, status) {
+    const photoUrl = results.photos[0].getUrl({maxWidth:600});
+    image.src = photoUrl;
+    imageInput.value = photoUrl;
+  }
 
   function getCoordsFromPostcode() {
     geocoder.geocode({ 'address': `${venueInput.value}, ${postcodeInput.value}` }, function(results, status) {
@@ -45,6 +54,8 @@ function initMap2() {
         map.setCenter(results[0].geometry.location);
         marker.setPosition(results[0].geometry.location);
         map.setCenter(marker.getPosition());
+        const placeId = results[0].place_id;
+        places.getDetails({ placeId: placeId }, setPhoto);
       } else {
         console.log("Uh oh", status);
       }
@@ -57,7 +68,6 @@ function initMap2() {
 
   function setInputValues() {
     latlng = marker.getPosition();
-    console.log(latlng);
     newlat=(Math.round(latlng.lat()*1000000))/1000000;
     newlng=(Math.round(latlng.lng()*1000000))/1000000;
     latInput.value = newlat;
