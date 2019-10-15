9dec613168a03ea531389be505296f260a0a3449
Kate Howard <k.l.howard.1992@gmail.com>
2019-10-15 18:55:38 +0100

    Remove image-related code

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -27,34 +27,14 @@ function initMap2(lat, lng) {
   const photoInput = document.getElementById('quiz_photo_url');
   const geocodeButton = document.getElementById('geocode_button');
   const selection = document.getElementById('image_selection');
-  const photo = document.getElementById('venue_photo');
 
   if (lat && lng) {
     initMap(lat, lng);
   }
 
-  function setPhotos(results, status) {
-    console.log(results);
-    for (i=0; i < results.photos.length; i++) {
-      const label = document.createElement('label');
-      const input = document.createElement('input');
-      input.type = "radio";
-      const image = document.createElement('img');
-      const photoUrl = results.photos[i].getUrl({maxHeight:200});
-      image.src = photoUrl;
-      label.appendChild(input);
-      label.appendChild(image);
-      selection.appendChild(label);
-      input.onclick = function() {
-        photo.src = photoUrl;
-        photoInput.value = photoUrl;
-      }
-    }
-  }
-
   function getCoordsFromPostcode() {
-    const geocoder = new google.maps.Geocoder;
-    geocoder.geocode({ 'address': `${venueInput.value}, ${postcodeInput.value}` }, function(results, status) {
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: `${venueInput.value}, ${postcodeInput.value}` }, function(results, status) {
       if (status == 'OK') {
         const coords = new google.maps.LatLng(51.45946, -2.5907347);
 
@@ -85,21 +65,20 @@ function initMap2(lat, lng) {
 
         const placeId = results[0].place_id;
         const places = new google.maps.places.PlacesService(map);
-        places.getDetails({ placeId: placeId }, setPhotos);
 
         setInputValues(marker.getPosition());
       } else {
-        console.log("Uh oh", status);
+        console.log('Uh oh', status);
       }
     });
   }
 
   function setInputValues(latlng) {
-    newlat=(Math.round(latlng.lat()*1000000))/1000000;
-    newlng=(Math.round(latlng.lng()*1000000))/1000000;
+    newlat = Math.round(latlng.lat() * 1000000) / 1000000;
+    newlng = Math.round(latlng.lng() * 1000000) / 1000000;
     latInput.value = newlat;
     lngInput.value = newlng;
-  };
+  }
 
   geocodeButton.addEventListener('click', function() {
     getCoordsFromPostcode();
