7aafe42a87072e0bc2d0ad6fd46d255ebbd5eb5a
Kate Howard <k.l.howard.1992@gmail.com>
2019-11-06 08:30:12 +0000

    Change price to float so entry fees can have 50ps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_222806) do
+ActiveRecord::Schema.define(version: 2019_11_06_082540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_10_30_222806) do
     t.string "day"
     t.string "frequency"
     t.string "time"
-    t.integer "price"
+    t.float "price"
     t.string "prize"
     t.string "status"
     t.string "link"
