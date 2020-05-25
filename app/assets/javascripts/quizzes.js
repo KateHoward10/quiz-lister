31f24c34c429e70ea96cd8048683b5559de2f191
Kate Howard <k.l.howard.1992@gmail.com>
2020-05-25 13:19:15 +0100

    Handle favourite number going back to 0

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -23,14 +23,14 @@ function displayMap(lat, lng) {
 
 function setFavourite() {
   const favouriteButton = document.getElementById('favourite_button');
+  const number = document.querySelector('.favourite-number');
 
   if (favouriteButton) {
     favouriteButton.addEventListener('click', () => {
       const icon = favouriteButton.firstElementChild;
-      const number = favouriteButton.querySelector('.favourite-number');
       const isUnfavouriting = icon.classList.contains('red');
       icon.classList.toggle('red');
-      if (number) number.textContent = +number.textContent + (isUnfavouriting ? -1 : 1);
+      if (number) number.textContent = ((+number.textContent || 0) + (isUnfavouriting ? -1 : 1)) || "";
     });
   }
 }
