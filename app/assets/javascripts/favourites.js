206d40fcc54796673f7dfc293fbf449a53e523e8
Kate Howard <k.l.howard.1992@gmail.com>
2020-06-09 18:27:13 +0100

    Fix (un)favourite buttons

--- app/assets/javascripts/favourites.js
+++ app/assets/javascripts/favourites.js
@@ -3,11 +3,21 @@ function setFavourite() {
   const number = document.querySelector('.favourite-number');
 
   if (favouriteButton) {
-    favouriteButton.addEventListener('click', () => {
+    favouriteButton.addEventListener('ajax:success', () => {
       const icon = favouriteButton.firstElementChild;
       const isUnfavouriting = icon.classList.contains('red');
       icon.classList.toggle('red');
       if (number) number.textContent = ((+number.textContent || 0) + (isUnfavouriting ? -1 : 1)) || "";
     });
+  }
+}
+
+function removeFavourite() {
+  const unfavouriteButton = document.querySelector('.unfavourite_button');
+
+  if (unfavouriteButton) {
+    unfavouriteButton.addEventListener('ajax:success', () => {
+      unfavouriteButton.parentElement.remove();
+    })
   }
 }
