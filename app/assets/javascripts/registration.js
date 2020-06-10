f313246ebd105b0c06bec3e5466ecc2b90536871
Kate Howard <k.l.howard.1992@gmail.com>
2020-06-10 12:23:07 +0100

    Provide feedback if password confirmation matches

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,6 +1,16 @@
 function passwordFeedback() {
   const passwordInput = document.getElementById('user_password');
-  if (passwordInput) passwordInput.addEventListener('input', e => {
-    passwordInput.style = `background-color: hsl(${Math.min(e.target.value.length * 10, 120)},100%,40%)`;
-  });
+  const passwordConfirmation = document.getElementById('user_password_confirmation');
+  const confirmIcons = document.querySelectorAll('.confirm-icon');
+
+  if (passwordInput) {
+    passwordInput.addEventListener('input', e => {
+      e.target.style = `background-color: hsl(${Math.min(e.target.value.length * 10, 120)},100%,40%)`;
+    });
+
+    if (passwordConfirmation && confirmIcons) passwordConfirmation.addEventListener('input', e => {
+      confirmIcons.forEach(icon => icon.style = `display: ${passwordInput.value === e.target.value ? 'flex' : 'none'}`);
+      e.target.style = `background-color: ${passwordInput.value === e.target.value ? `hsl(${Math.min(e.target.value.length * 10, 120)},100%,40%)` : '#fff'}`;
+    });
+  }
 }
