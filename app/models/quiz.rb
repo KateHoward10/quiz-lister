beffbf72a8654794533f8ff31f860ccd23875d32
Kate Howard <k.l.howard.1992@gmail.com>
2020-06-13 19:12:43 +0100

    Update form validation and styling

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,7 @@
 class Quiz < ApplicationRecord
   acts_as_favoritable
   belongs_to :user, optional: true
-  validates :venue, presence: true
+  validates :venue, :postcode, :latitude, :longitude, presence: true
+  validates :prize, length: { maximum: 200 }
   validates :display_email, acceptance: { message: "To create a new quiz, you must agree to display a contact email." }
 end
