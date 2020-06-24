55b081ea5196c73942344bc34e54ab3f04badd82
Kate Howard <k.l.howard.1992@gmail.com>
2020-06-24 15:47:45 +0100

    Add date array

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_182855) do
+ActiveRecord::Schema.define(version: 2020_06_24_144407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 2020_06_07_182855) do
     t.decimal "longitude"
     t.text "address"
     t.integer "hue"
-    t.string "contact_email"
     t.integer "user_id"
+    t.date "dates", default: [], array: true
   end
 
   create_table "users", force: :cascade do |t|
