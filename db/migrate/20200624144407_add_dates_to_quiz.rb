cc5c57c152d256d6c1fe9d610cd3b489d55cbe2b
Kate Howard <k.l.howard.1992@gmail.com>
2020-06-26 14:38:35 +0100

    Remove attempted removal of contact_email

--- db/migrate/20200624144407_add_dates_to_quiz.rb
+++ db/migrate/20200624144407_add_dates_to_quiz.rb
@@ -1,6 +1,5 @@
 class AddDatesToQuiz < ActiveRecord::Migration[5.2]
   def change
     add_column :quizzes, :dates, :date, array: true, default: []
-    remove_column :quizzes, :contact_email
   end
 end
