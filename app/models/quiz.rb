2e10cfc875f7c5ade6ca4e7f49af9d9e05879e69
Kate Howard <k.l.howard.1992@gmail.com>
2020-07-02 14:38:13 +0100

    More validation on quiz

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,6 +2,12 @@ class Quiz < ApplicationRecord
   acts_as_favoritable
   belongs_to :user, optional: true
   validates :venue, :postcode, :latitude, :longitude, presence: true
+  validates_uniqueness_of :venue
+  validate :check_bristol_postcode
   validates :prize, length: { maximum: 200 }
   validates :display_email, acceptance: { message: "To create a new quiz, you must agree to display a contact email." }
+
+  def check_bristol_postcode
+    errors.add(:postcode, "must be a valid Bristol postcode") unless postcode.downcase.start_with?('BS')
+  end
 end
