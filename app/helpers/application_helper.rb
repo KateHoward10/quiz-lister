4a429cec0ffb958ae635b6fc7910c2ef3f20e42e
Kate Howard <k.l.howard.1992@gmail.com>
2020-07-05 11:16:26 +0100

    Fix hover for account menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user)
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?d=identicon"
+    image_tag(gravatar_url, size: "28x28")
+  end
 end
