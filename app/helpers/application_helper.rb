fbb7fe3dfd2946d5f5ffbdc3017690242e986e73
Kate Howard <k.l.howard.1992@gmail.com>
2020-07-06 18:50:04 +0100

    Avatars with initials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
-  def gravatar_for(user)
-    email_address = user.email.downcase
-    hash = Digest::MD5.hexdigest(email_address)
-    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?d=identicon"
-    image_tag(gravatar_url, size: "28x28")
+  def avatar_for(user)
+    name = user.username || user.email
+    colours = ["4B0082", "8B008B", "9400D3", "8A2BE2", "483D8B", "191970", "0000CD", "008080", "006400", "2F4F4F"]
+    colour = colours[name.length % colours.length]
+    avatar_url = "https://eu.ui-avatars.com/api/?name=#{name}&length=1&rounded=true&bold=true&background=#{colour}&color=fff"
+    image_tag(avatar_url, size: "28x28")
   end
 end
