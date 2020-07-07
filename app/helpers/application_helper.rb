a1992e39af6c0516df659493679a752dbb140187
Kate Howard <k.l.howard.1992@gmail.com>
2020-07-07 13:37:10 +0100

    Display avatar with CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,10 @@ module ApplicationHelper
     name = user.username || user.email
     colours = ["4B0082", "8B008B", "9400D3", "8A2BE2", "483D8B", "191970", "0000CD", "008080", "006400", "2F4F4F"]
     colour = colours[name.length % colours.length]
-    avatar_url = "https://eu.ui-avatars.com/api/?name=#{name}&length=1&rounded=true&bold=true&background=#{colour}&color=fff"
-    image_tag(avatar_url, size: "28x28")
+    html = <<-HTML
+      <div class="avatar" style="background: ##{colour}">#{name[0].upcase}</div>
+    HTML
+
+    html.html_safe
   end
 end
