a8ada1831bf14dd6a8119c5997d330fc5e001993
Kate Howard <k.l.howard.1992@gmail.com>
2020-07-15 19:50:02 +0100

    List Heroku as domain

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,16 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Initialize the Rails application.
-Rails.application.initialize!
-
 ActionMailer::Base.smtp_settings = {
+  from: ENV['GMAIL_USERNAME'],
   address: 'smtp.sendgrid.net',
   port: 587,
   authentication: 'plain',
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
-  domain: 'brizquizlist.co.uk',
+  domain: 'brizquizlist.herokuapp.com',
   enable_starttls_auto: true
 }
+
+# Initialize the Rails application.
+Rails.application.initialize!
