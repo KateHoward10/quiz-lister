0da0aec145bb27ecde2da6e89129a567a33aa050
Kate Howard <k.l.howard.1992@gmail.com>
2020-07-30 14:10:44 +0100

    Use LetsEncrypt

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,8 @@
-class StaticController < ApplicationController
+class StaticController < ApplicationController  
+  def letsencrypt
+    render text: "#{ENV['LETS_ENCRYPT_KEY']}"
+  end
+
   def show
     @quizzes = Quiz.all
     @locations = Quiz.select(:id, :venue, :latitude, :longitude)
