9af56a04d4fad345ba18019cb81e777759dd0011
Kate Howard <k.l.howard.1992@gmail.com>
2020-08-08 13:15:03 +0100

    Update password strength indicator

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -3,14 +3,18 @@ function passwordFeedback() {
   const passwordConfirmation = document.getElementById('user_password_confirmation');
   const confirmIcons = document.querySelectorAll('.confirm-icon');
 
+  const labels = ["Weak", "OK", "Good", "Strong"];
+
   if (passwordInput) {
-    passwordInput.addEventListener('input', e => {
-      e.target.style = `background-color: hsl(${Math.min(e.target.value.length * 10, 120)},100%,60%)`;
+    passwordInput.addEventListener('input', ({ target: { value } }) => {
+      const indicator = passwordInput.nextElementSibling;
+      const strength = Math.min(Math.floor(value.length / 4), 3);
+      indicator.style = `background-color: hsl(${strength * 40},100%,40%); width: ${((strength + 1) * 25) - 1}%`;
+      indicator.textContent = labels[strength];
     });
 
     if (passwordConfirmation && confirmIcons) passwordConfirmation.addEventListener('input', e => {
       confirmIcons.forEach(icon => icon.style = `display: ${passwordInput.value === e.target.value ? 'inline' : 'none'}`);
-      e.target.style = `background-color: ${passwordInput.value === e.target.value ? `hsl(${Math.min(e.target.value.length * 10, 120)},100%,60%)` : '#fff'}`;
     });
   }
 }
