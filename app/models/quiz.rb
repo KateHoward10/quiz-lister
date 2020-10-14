c24b65964eeb6d62db29f61663bfdcb52b72783d
Kate Howard <kate@simpleweb.co.uk>
2020-10-14 13:36:32 +0100

    Ensure venue names are unique and provide helpful error message

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,7 +3,7 @@ class Quiz < ApplicationRecord
   belongs_to :user, optional: true
   has_many :events, dependent: :destroy
   validates :venue, :postcode, :latitude, :longitude, presence: true
-  validates_uniqueness_of :venue
+  validates_uniqueness_of :venue, case_sensitive: false, message: "name has already been taken. Consider adding an area to distinguish your venue from somewhere else with the same name, e.g. 'The Royal Oak, Redland'."
   validate :check_bristol_postcode
   validates :prize, length: { maximum: 200 }
   validates :display_email, acceptance: { message: "To create a new quiz, you must agree to display a contact email." }
