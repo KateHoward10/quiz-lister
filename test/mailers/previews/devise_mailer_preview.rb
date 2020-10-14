fd5ef8c392eade76bd6e3eb7d9b76ad5f441f486
Kate Howard <kate@simpleweb.co.uk>
2020-10-14 11:41:21 +0100

    Update email copy

--- test/mailers/previews/devise_mailer_preview.rb
+++ test/mailers/previews/devise_mailer_preview.rb
@@ -1,26 +1,21 @@
 class DeviseMailerPreview < ActionMailer::Preview
-  # hit http://localhost:3000/rails/mailers/devise_mailer/email_changed
+  # http://localhost:3000/rails/mailers/devise_mailer/email_changed
   def email_changed
     DeviseMailer.email_changed(User.first, {})
   end
 
-  # hit http://localhost:3000/rails/mailers/devise_mailer/confirmation_instructions
+  # http://localhost:3000/rails/mailers/devise_mailer/confirmation_instructions
   def confirmation_instructions
     DeviseMailer.confirmation_instructions(User.first, {})
   end
 
-  # hit http://localhost:3000/rails/mailers/devise_mailer/password_change
+  # http://localhost:3000/rails/mailers/devise_mailer/password_change
   def password_change
     DeviseMailer.password_change(User.first, {})
   end
 
-  # hit http://localhost:3000/rails/mailers/devise_mailer/reset_password_instructions
+  # http://localhost:3000/rails/mailers/devise_mailer/reset_password_instructions
   def reset_password_instructions
     DeviseMailer.reset_password_instructions(User.first, {})
-  end
-
-  # hit http://localhost:3000/rails/mailers/devise_mailer/unlock_instructions
-  def unlock_instructions
-    DeviseMailer.unlock_instructions(User.first, {})
   end
 end
