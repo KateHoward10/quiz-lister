607ee697236d087206a523a84f8af9bfa687dc16
Kate Howard <kate@simpleweb.co.uk>
2020-10-14 15:44:29 +0100

    Enable delete user button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,15 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_path, notice: "User was successfully deleted." }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
   def require_admin!
