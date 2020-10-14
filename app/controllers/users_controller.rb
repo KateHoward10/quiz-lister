25be068e7b51fb148809d1e85043c28fd807e07e
Kate Howard <kate@simpleweb.co.uk>
2020-10-14 15:29:22 +0100

    Restrict users access to admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+  before_action :require_admin!
+
   def index
     @users = User.all
+  end
+
+  private
+
+  def require_admin!
+    if !current_user.try(:admin?)
+      redirect_to quizzes_path
+      flash[:alert] = "You are not authorised to perform this action."
+    end
   end
 end
