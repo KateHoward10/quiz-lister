82e1ea778de351f1afa008bd19d9bce0915c1f8b
Kate Howard <k.l.howard.1992@gmail.com>
2020-11-03 22:57:38 +0000

    Fix users page error

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get "/map" => "quizzes#map"
 
   root "quizzes#index"
-  get "/:page" => "static#show"
-
   resources :users
+
+  get "/:page" => "static#show"
 end
