376747501fbc06954f25157bd42ce513571256b5
Kate Howard <k.l.howard.1992@gmail.com>
2021-05-29 15:24:41 +0100

    Redirect to www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_root_domain
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -6,5 +7,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  private
+
+  def redirect_root_domain
+    return unless request.host === 'brizquizlist.co.uk'
+    redirect_to("#{request.protocol}#{ENV['HOST_URL']}#{request.fullpath}", status: 301)
   end
 end
